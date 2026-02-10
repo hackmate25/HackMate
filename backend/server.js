b0f8c8de84db865ac4a67579bb24907184adc3ae
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2026-02-10 10:06:52 +0530

    minor cors

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 
 // ✅ Allow multiple origins
 const allowedOrigins = [
-  "https://hack-mate-ten.vercel.app",
+  "https://hackmate-official.vercel.app",
   "http://localhost:5173"
 ];
 
